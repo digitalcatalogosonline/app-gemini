9bf98bb78ce7204ff8ffe50a6b1b8b61c159af68
SAMUEL <samuelpego562@gmail.com>
2025-01-09 22:58:08 -0500

    adding loader

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 "use client"
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
   const [data, setData] = useState("");
   const [inputValue, setInputValue] = useState("");
   const [active, setActive] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   const stylesTextArea =
     { resize: "none", height: 80, border: "none", outline: active ? "none" : "none", flexGrow: "1", padding: 10 }
@@ -15,13 +17,16 @@ export default function Home() {
     try {
       e.preventDefault();
       const value = inputValue;
+      setLoading(true)
       const req = await fetch(`/api`, { method: "POST", body: JSON.stringify({ prompt: value }), headers: { "Content-Type": "application/json" } });
       const res = await req.json()
       setData(res.result)
-
+      setLoading(false)
     } catch (error) {
+      setLoading(false)
       console.log(error)
       alert(error)
+
     }
   };
   useEffect(() => {
@@ -32,8 +37,9 @@ export default function Home() {
 
   return (
     <>
-      <div style={{ width: "80%", height: "65vh", margin: "0 auto", background: "#ddd", padding: "20px", border: "1px solid #ccc", borderRadius: "10px", overflowY: "auto" }}>
+      <div style={{ width: "80%", height: "65vh", margin: "0 auto", background: "#ddd", padding: "20px", border: "1px solid #ccc", borderRadius: "10px", overflowY: "auto", position: "relative" }}>
         <pre style={{ whiteSpace: "pre-wrap", fontSize: "1.1rem" }} dangerouslySetInnerHTML={{ __html: data }}></pre>
+        <Image src="/watch-loader.svg" alt="" width={100} height={100} style={{ display: !loading ? "none" : "inline", position: "absolute", left: "50%", top: "50%", transform: "translate(-50%,-50%)" }} />
       </div>
 
       <div style={{ borderRadius: "20px", display: "flex", alignItems: "center", gap: "10px", boxShadow: "4px 4px 10px 4px #ccc", width: "80%", background: "#f8f4f2", padding: "20px", position: "fixed", bottom: "20px", left: "50%", transform: "translateX(-50%)" }}>
